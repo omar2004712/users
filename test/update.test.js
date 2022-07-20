7b829e67cb821eb608c6fe3872102d86f49a3e2a
omar2004712 <o.bahutair@gmail.com>
2022-07-20 11:28:52 +0400

    test (update): tested the update instance model

--- test/update.test.js
+++ test/update.test.js
@@ -10,15 +10,24 @@ describe("Updates records in the database", () => {
     joe.save().then(() => done());
   });
 
-  it("instance type using set n save", (done) => {
-    joe.set("name", "Alex");
-    joe
-      .save()
-      .then(() => User.find({}))
+  //to check if the updates of the name worked
+  function assertName(operation, done) {
+    // operation is a promise returned by any operation done on the DB e.g: updateOne, save
+    operation
+      .then(() => User.find({})) // to ge all the users inside the DB
       .then((users) => {
         assert.strictEqual(users.length, 1); // to check if there is more than one user in the DB
         assert.strictEqual(users[0].name, "Alex"); // to check if the name of the user in the DB is Alex
         done();
       });
+  }
+
+  it("instance type using set n save", (done) => {
+    joe.set("name", "Alex");
+    assertName(joe.save(), done);
+  });
+
+  it("instance updateOne", (done) => {
+    assertName(joe.updateOne({ name: "Alex" }), done);
   });
 });
