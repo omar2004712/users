270bfb25f55e3b2d8ee3080472d0894d5762615d
omar2004712 <o.bahutair@gmail.com>
2022-07-21 17:39:16 +0400

    test (subdocument): tested the addition and deletion of posts

--- test/subdocument.test.js
+++ test/subdocument.test.js
@@ -1,3 +1,60 @@
 const assert = require("assert");
 
 const User = require("../src/user");
+
+describe("Subdocuments", () => {
+  it("can create a subdocument", (done) => {
+    const joe = new User({
+      name: "Joe",
+      posts: [{ title: "PostTitle" }],
+    });
+
+    joe
+      .save()
+      .then(() => User.findOne({ name: "Joe" }))
+      .then((user) => {
+        assert.strictEqual(user.posts[0].title, "PostTitle");
+        done();
+      });
+  });
+
+  it("Can add subdocuments to an existing record", (done) => {
+    const joe = new User({
+      name: "Joe",
+      posts: [],
+    });
+
+    joe
+      .save()
+      .then(User.findOne({ name: "Joe" }))
+      .then((user) => {
+        user.posts.push({ title: "new Title" });
+        return user.save();
+      })
+      .then(() => User.findOne({ name: "Joe" }))
+      .then((user) => {
+        assert.strictEqual(user.posts[0].title, "new Title");
+        done();
+      });
+  });
+
+  it("deletes a post", (done) => {
+    const joe = new User({
+      name: "Joe",
+      posts: [{ title: "New Post" }],
+    });
+
+    joe
+      .save()
+      .then(() => User.findOne({ name: "Joe" }))
+      .then((user) => {
+        user.posts[0].remove();
+        return user.save();
+      })
+      .then(() => User.findOne({ name: "Joe" }))
+      .then((user) => {
+        assert.strictEqual(user.posts.length, 0);
+        done();
+      });
+  });
+});
