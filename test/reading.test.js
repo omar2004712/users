397d29792736ff5e6296ff01e1e249b76070843b
omar2004712 <o.bahutair@gmail.com>
2022-07-23 14:07:52 +0400

    test (reading): tested the skip limit result featrue
    created multiple users and selected a some amount of them using skip and
    limit

--- test/reading.test.js
+++ test/reading.test.js
@@ -1,12 +1,21 @@
 const assert = require("assert");
+const mongoose = require("mongoose");
 const User = require("../src/user");
 
 describe("reading users out of the database", () => {
-  let joe; // to make it available in the it test
+  let joe, alex, maria, zach; // to make it available in the it test
 
   beforeEach((done) => {
     joe = new User({ name: "Joe" });
-    joe.save().then(() => done());
+    alex = new User({ name: "Alex" });
+    maria = new User({ name: "Maria" });
+    zach = new User({ name: "Zach" });
+
+    Promise.all([joe.save(), zach.save(), alex.save(), maria.save()]).then(
+      () => {
+        done();
+      }
+    );
   });
 
   it("finds all user with the name of joe", (done) => {
@@ -21,5 +30,18 @@ describe("reading users out of the database", () => {
       assert.strictEqual(user.name, "Joe");
       done();
     });
+  });
+
+  it("skips and limits the result list", (done) => {
+    User.find({})
+      .sort({ name: 1 })
+      .skip(1)
+      .limit(2)
+      .then((users) => {
+        assert.strictEqual(users.length, 2);
+        assert.strictEqual(users[0].name, "Joe");
+        assert.strictEqual(users[1].name, "Maria");
+        done();
+      });
   });
 });
